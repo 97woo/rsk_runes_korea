2d72b91c6d43c352b4712670ba72b6e8ca271b2e
97woo <kicaptain16@gmail.com>
2025-02-26 16:23:18 +0900

    fix: env.sample contract address

--- contracts/RuneToken.sol
+++ contracts/RuneToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/Strings.sol';
 
 contract RuneToken is ERC1155, Ownable {
     event TokensFrozen(address indexed account, uint256 indexed tokenId, uint256 amount);
